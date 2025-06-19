1114b0278d38e07577e09ae0317dff50d7676565
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-06-19 11:42:33 +0300

    main and bonus

---
+++ bonus.py
@@ -0,0 +1,22 @@
+
+
+
+def function(number:int):
+    finalResult= ''
+    for i in range(number, 0, -1):
+        result = ''
+        for j in range(i, 0,-1):
+            result += str(j) + " " 
+        finalResult += result + "\n"
+    return finalResult
+    
+       
+       
+   
+
+       
+
+function(5)
+    
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+def function(number:int):
+    for i in range(number, 0, -1):
+        result = ''
+        for j in range(i, 0,-1):
+            result += str(j) + " " 
+        print(result)
+       
+    
+       
+       
+    
+            
+    
+
+
+
+
+
+
+function(5)
